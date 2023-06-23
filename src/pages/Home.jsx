16749957ec2a94d434b60e91bb524c58248676a6
Mennat Allah <manoshkamal316@gmail.com>
2023-06-24 02:35:11 +0300

    edit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,56 @@
 function Home() {
   return (
     <>
-      <h1>Home Page</h1>
+      <div className="flex gap-6 justify-center">
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+        <div className="bg-[#EBF3FE] w-[14%] p-8 rounded flex flex-col justify-center items-center text-[#2100C4] text-[14px] font-[600]">
+          <img
+            src="https://demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/dist/images/svgs/icon-user-male.svg"
+            alt=""
+          />
+          <h3>Users</h3>
+          <h3>50</h3>
+        </div>
+      </div>
     </>
   );
 }
